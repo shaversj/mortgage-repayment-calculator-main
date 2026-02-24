8f7cbb9eff0a59c7305c8d31fb995254984c37cd
Josh Shavers <shaversj@gmail.com>
2026-02-24 07:45:31 -0600

    Add basic elements to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,139 +1,257 @@
 import { useRef, useState } from "react";
-import { Autocomplete, createTheme, Loader } from "@mantine/core";
+import { Title, Text, Flex, Image, Box, TextInput, ThemeIcon, Fieldset, Radio, Button } from "@mantine/core";
+import { theme } from "./theme/theme.ts";
 import "./App.css";
 import "@mantine/core/styles.css";
 
+import illustrion from "./assets/images/illustration-empty.svg";
+import calculator from "./assets/images/icon-calculator.svg";
+import { PiCurrencyGbpBold } from "react-icons/pi";
+
 import { MantineProvider } from "@mantine/core";
-
-export const theme = createTheme({
-  fontFamily: "Plus Jakarta Sans, sans-serif",
-  headings: {
-    fontFamily: "Plus Jakarta Sans, Inter, sans-serif",
-    fontWeight: "700",
-    sizes: {
-      h1: {
-        fontSize: "56px",
-        lineHeight: "1.25",
-      },
-      h2: {
-        fontSize: "24px",
-        lineHeight: "1.25",
-      },
-      h3: {
-        fontSize: "18px",
-        lineHeight: "1.25",
-      },
-    },
-  },
-  fontSizes: {
-    xs: "14px", // text preset 5
-    sm: "16px", // text preset 4
-    md: "18px",
-    lg: "24px",
-    xl: "56px",
-  },
-
-  lineHeights: {
-    xs: "1.5",
-    sm: "1.5",
-    md: "1.25",
-    lg: "1.25",
-    xl: "1.25",
-  },
-
-  spacing: {
-    xs: "8px",
-    sm: "12px",
-    md: "16px",
-    lg: "24px",
-    xl: "32px",
-    xxl: "40px",
-  },
-  primaryColor: "lime",
-  colors: {
-    lime: [
-      "#f9fae3",
-      "#f1f3b5",
-      "#e9ec86",
-      "#e1e558",
-      "#d8db2f", // 4 (your base)
-      "#c4c81a",
-      "#a7ad14",
-      "#8a910f",
-      "#6e740a",
-      "#525706",
-    ],
-
-    slate: [
-      "#E4F4FD", // 0
-      "#cfe2ef",
-      "#b8cfdf",
-      "#9ABED5", // 3
-      "#82abc4",
-      "#6B94A8", // 5
-      "#567b8e",
-      "#4E6E7E", // 7
-      "#2e4d60",
-      "#133041", // 9
-    ],
-
-    red: [
-      "#fde8e6",
-      "#f8c5c1",
-      "#f2a09b",
-      "#ec7b75",
-      "#e4564f",
-      "#d73328", // base
-      "#b92a20",
-      "#9c2219",
-      "#7e1a12",
-      "#61120c",
-    ],
-  },
-
-  primaryShade: {
-    light: 4,
-    dark: 5,
-  },
-
-  white: "#FFFFFF",
-  black: "#133041",
-
-  defaultRadius: "md",
-});
 
 function App() {
   const timeoutRef = useRef<number>(-1);
   const [value, setValue] = useState("");
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<string[]>([]);
+  const [mortgageType, setMortgageType] = useState<"repayment" | "interest-only">("repayment");
 
-  const handleChange = (val: string) => {
-    window.clearTimeout(timeoutRef.current);
-    setValue(val);
-    setData([]);
+  const calculatorIcon = <Image src={calculator} alt="Calculate Icon" h={24} w={24} />;
 
-    if (val.trim().length === 0 || val.includes("@")) {
-      setLoading(false);
-    } else {
-      setLoading(true);
-      timeoutRef.current = window.setTimeout(() => {
-        setLoading(false);
-        setData(["gmail.com", "outlook.com", "yahoo.com"].map((provider) => `${val}@${provider}`));
-      }, 1000);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+
+    if (timeoutRef.current !== -1) {
+      clearTimeout(timeoutRef.current);
     }
+
+    timeoutRef.current = window.setTimeout(() => {
+      setLoading(true);
+      // Simulate an API call
+      setTimeout(() => {
+        setData([newValue]); // Replace with actual data from API
+        setLoading(false);
+      }, 1000);
+    }, 500);
   };
 
+  const themedPi = (
+    <ThemeIcon
+      color={theme.colors?.slate?.[1]}
+      w={60}
+      h={48}
+      style={{
+        border: `1px solid ${theme.colors?.slate?.[5]}`,
+        borderTopLeftRadius: "4px",
+        borderBottomLeftRadius: "4px",
+        borderLeft: "none",
+      }}
+      radius={0}
+    >
+      <PiCurrencyGbpBold size={16} color={theme.colors?.slate?.[5]}></PiCurrencyGbpBold>
+    </ThemeIcon>
+  );
   return (
     <MantineProvider theme={theme}>
-      <Autocomplete
-        value={value}
-        data={data}
-        onChange={handleChange}
-        rightSection={loading ? <Loader size={16} /> : null}
-        label="Async Autocomplete data"
-        placeholder="Your email"
-      />
+      <Flex h="100vh" w="100vw" justify="center" align="center">
+        <Box w={540} h={606} py="xl" p={40}>
+          <Flex align="center" justify="space-between">
+            <Title size="h2">Mortgage Calculator</Title>
+            <Box ml="auto">
+              <Text size="xs" td="underline">
+                Clear All
+              </Text>
+            </Box>
+          </Flex>
+
+          <form style={{ paddingTop: "40px" }}>
+            <TextInput
+              styles={{
+                input: { paddingLeft: "56px", fontSize: "18px", fontWeight: 700, lineHeight: "125%", borderRadius: "4px", height: 48, border: `1px solid ${theme.colors?.slate?.[5]}` },
+              }}
+              leftSectionPointerEvents="none"
+              leftSection={themedPi}
+              leftSectionWidth="40px"
+              label="Mortgage Amount"
+              labelProps={{
+                style: { paddingBottom: "12px", color: theme.colors?.slate?.[7], fontSize: "16px", fontWeight: 500, lineHeight: "150%" },
+              }}
+              value={value}
+              onChange={handleChange}
+            />
+
+            <Flex style={{ paddingTop: "24px" }} gap={24}>
+              <TextInput
+                styles={{
+                  input: {
+                    paddingLeft: "16px",
+                    paddingRight: "16px",
+                    fontSize: "18px",
+                    fontWeight: 700,
+                    lineHeight: "125%",
+                    borderRadius: "4px",
+                    height: 48,
+                    border: `1px solid ${theme.colors?.slate?.[5]}`,
+                  },
+                }}
+                rightSectionPointerEvents="none"
+                rightSectionWidth={80}
+                rightSection={
+                  <Text
+                    h={48}
+                    w={80}
+                    c={theme.colors?.slate?.[5]}
+                    style={{
+                      paddingRight: "16x",
+                      paddingLeft: "16px",
+                      paddingTop: "12px",
+                      paddingBottom: "12px",
+                      backgroundColor: theme.colors?.slate?.[1],
+                      border: `1px solid ${theme.colors?.slate?.[5]}`,
+                      borderTopRightRadius: "4px",
+                      borderBottomRightRadius: "4px",
+                      borderLeft: "none",
+                      borderRight: "none",
+                      fontSize: "18px",
+                      fontWeight: 700,
+                      letterSpacing: "-1px",
+                      lineHeight: "125%",
+                    }}
+                  >
+                    years
+                  </Text>
+                }
+                label="Mortgage Term"
+                labelProps={{
+                  style: { paddingBottom: "12px", color: theme.colors?.slate?.[7], fontSize: "16px", fontWeight: 500, lineHeight: "150%" },
+                }}
+              />
+
+              <TextInput
+                styles={{
+                  input: {
+                    paddingLeft: "16px",
+                    paddingRight: "16px",
+                    fontSize: "18px",
+                    fontWeight: 700,
+                    lineHeight: "125%",
+                    borderRadius: "4px",
+                    height: 48,
+                    border: `1px solid ${theme.colors?.slate?.[5]}`,
+                  },
+                }}
+                rightSectionPointerEvents="none"
+                rightSectionWidth={51}
+                rightSection={
+                  <Text
+                    h={48}
+                    w={51}
+                    c={theme.colors?.slate?.[5]}
+                    style={{
+                      paddingRight: "16x",
+                      paddingLeft: "16px",
+                      paddingTop: "12px",
+                      paddingBottom: "12px",
+                      backgroundColor: theme.colors?.slate?.[1],
+                      border: `1px solid ${theme.colors?.slate?.[5]}`,
+                      borderTopRightRadius: "4px",
+                      borderBottomRightRadius: "4px",
+                      borderLeft: "none",
+                      borderRight: "none",
+                      fontSize: "18px",
+                      fontWeight: 700,
+                      letterSpacing: "-1px",
+                      lineHeight: "125%",
+                    }}
+                  >
+                    %
+                  </Text>
+                }
+                label="Interest Rate"
+                labelProps={{
+                  style: { paddingBottom: "12px", color: theme.colors?.slate?.[7], fontSize: "16px", fontWeight: 500, lineHeight: "150%" },
+                }}
+              />
+            </Flex>
+
+            <Fieldset
+              legend="Mortgage Type"
+              w="100%"
+              style={{
+                color: theme.colors?.slate?.[7],
+                fontSize: "16px",
+                fontWeight: 500,
+                lineHeight: "150%",
+                border: "none",
+                paddingLeft: 0,
+                paddingRight: 0,
+                paddingBottom: "12px",
+                marginTop: "24px",
+              }}
+            >
+              <Flex gap={12} direction="column">
+                <Radio
+                  style={{
+                    border: `1px solid ${theme.colors?.slate?.[5]}`,
+                    borderRadius: "4px",
+                    padding: "12.5px 16px",
+                    fontSize: "18px",
+                    fontWeight: 700,
+                    lineHeight: "125%",
+                  }}
+                  c={theme.colors?.slate?.[9]}
+                  value="repayment"
+                  label="Repayment"
+                  checked={mortgageType === "repayment"}
+                  onChange={() => setMortgageType("repayment")}
+                />
+                <Radio
+                  style={{
+                    border: `1px solid ${theme.colors?.slate?.[5]}`,
+                    borderRadius: "4px",
+                    padding: "12.5px 16px",
+                    fontSize: "18px",
+                    fontWeight: 700,
+                    lineHeight: "125%",
+                  }}
+                  c={theme.colors?.slate?.[9]}
+                  value="interest-only"
+                  label="Interest Only"
+                  checked={mortgageType === "interest-only"}
+                  onChange={() => setMortgageType("interest-only")}
+                />
+              </Flex>
+            </Fieldset>
+
+            <Button
+              mt={30}
+              size={"lg"}
+              justify={"left"}
+              leftSection={calculatorIcon}
+              rightSection={<span />}
+              c={theme.colors?.slate?.[9]}
+              radius={"xl"}
+              style={{ fontSize: "18px", fontWeight: 700, lineHeight: "125%" }}
+              type="submit"
+            >
+              Calculate Repayments
+            </Button>
+          </form>
+        </Box>
+        <Box w={540} h={606} style={{ backgroundColor: theme.colors?.slate?.[9] }}>
+          <Flex h="100%" justify="center" align="center" direction="column" gap="sm" p={40}>
+            <Image h={192} w={192} src={illustrion} alt={"Illustration"} />
+            <Text size="lg" c="white" fw="700">
+              Results shown here
+            </Text>
+            <Text ta="center" size="md" c={theme.colors?.slate?.[5]}>
+              Complete the form and click “calculate repayments” to see what your monthly repayments would be.
+            </Text>
+          </Flex>
+        </Box>
+      </Flex>
     </MantineProvider>
   );
 }
---
+++ src/theme/theme.ts
@@ -0,0 +1,96 @@
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+  fontFamily: "Plus Jakarta Sans, sans-serif",
+  headings: {
+    fontFamily: "Plus Jakarta Sans, Inter, sans-serif",
+    fontWeight: "700",
+    sizes: {
+      h1: {
+        fontSize: "56px",
+        lineHeight: "1.25",
+      },
+      h2: {
+        fontSize: "24px",
+        lineHeight: "1.25",
+      },
+      h3: {
+        fontSize: "18px",
+        lineHeight: "1.25",
+      },
+    },
+  },
+  fontSizes: {
+    xs: "14px", // text preset 5
+    sm: "16px", // text preset 4
+    md: "18px",
+    lg: "24px",
+    xl: "56px",
+  },
+
+  lineHeights: {
+    xs: "1.5",
+    sm: "1.5",
+    md: "1.25",
+    lg: "1.25",
+    xl: "1.25",
+  },
+
+  spacing: {
+    xs: "8px",
+    sm: "12px",
+    md: "16px",
+    lg: "24px",
+    xl: "32px",
+    xxl: "40px",
+  },
+  primaryColor: "lime",
+  colors: {
+    lime: [
+      "#f9fae3",
+      "#f1f3b5",
+      "#e9ec86",
+      "#e1e558",
+      "#d8db2f", // 4 (your base)
+      "#c4c81a",
+      "#a7ad14",
+      "#8a910f",
+      "#6e740a",
+      "#525706",
+    ],
+
+    slate: [
+      "#E4F4FD", // 0
+      "#cfe2ef",
+      "#b8cfdf",
+      "#9ABED5", // 3
+      "#82abc4",
+      "#6B94A8", // 5
+      "#567b8e",
+      "#4E6E7E", // 7
+      "#2e4d60",
+      "#133041", // 9
+    ],
+
+    red: [
+      "#fde8e6",
+      "#f8c5c1",
+      "#f2a09b",
+      "#ec7b75",
+      "#e4564f",
+      "#d73328", // base
+      "#b92a20",
+      "#9c2219",
+      "#7e1a12",
+      "#61120c",
+    ],
+  },
+
+  primaryShade: {
+    light: 4,
+    dark: 5,
+  },
+
+  white: "#FFFFFF",
+  black: "#133041",
+});
