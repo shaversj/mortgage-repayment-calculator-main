e77c7b1226aff8c57c024ceefd5529b2a4f723df
Josh Shavers <shaversj@gmail.com>
2026-04-23 13:34:58 -0500

    Tanstack + Tailwind Setup

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    tanstackStart(),
+    // react's vite plugin must come after start's vite plugin
+    viteReact(),
+    tailwindcss(),
+  ],
+});
