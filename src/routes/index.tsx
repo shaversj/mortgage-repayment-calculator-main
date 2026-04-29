e352b1c937125aa26ff34c7919de037b499ea203
Josh Shavers <shaversj@gmail.com>
2026-04-29 10:37:35 -0500

    Update section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -160,16 +160,59 @@ function Home() {
           </main>
         </section>
 
-        <section className={"grid w-126 place-items-center rounded-br-3xl rounded-bl-[5rem] bg-slate-900"}>
-          <div className={"grid place-items-center space-y-4 p-10"}>
-            <img src={heroImage} alt="Illustration of a person calculating their mortgage repayments" />
-            <p className={"text-preset-2 text-white"}>Results shown here</p>
-            <p className={"text-preset-4 text-center text-slate-300"}>
-              Complete the form and click “calculate repayments” to see what your monthly repayments would be.
+        {/*<section className={"grid w-126 place-items-center rounded-br-3xl rounded-bl-[5rem] bg-slate-900"}>*/}
+        {/*  <div className={"grid place-items-center space-y-4 p-10"}>*/}
+        {/*    <img src={heroImage} alt="Illustration of a person calculating their mortgage repayments" />*/}
+        {/*    <p className={"text-preset-2 text-white"}>Results shown here</p>*/}
+        {/*    <p className={"text-preset-4 text-center text-slate-300"}>*/}
+        {/*      Complete the form and click “calculate repayments” to see what your monthly repayments would be.*/}
+        {/*    </p>*/}
+        {/*  </div>*/}
+        {/*</section>*/}
+
+        <section className={"w-126 rounded-br-3xl rounded-bl-[5rem] bg-slate-900 p-10"}>
+          <div className={""}>
+            <p className={"text-preset-2 text-white"}>Your Results</p>
+            <p className={"text-preset-4 text-slate-300"}>
+              Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate
+              repayments” again.
             </p>
+          </div>
+
+          <div className={"bg-lime mt-10 rounded-lg pt-1"}>
+            <div className={"rounded-t-[0.28888rem] rounded-b-lg bg-[#0e2431] px-8 py-10"}>
+              <p className={"text-preset-4 text-slate-300"}>Your monthly repayments</p>
+              <p className={"text-lime text-preset-1 pt-2"}>£1,797.74</p>
+              <div className={"mt-8 h-[1px] bg-[#9ABED5]/25"} />
+              <p className={"text-preset-4 pt-8 text-slate-300"}>Total you'll repay over the term</p>
+              <p className={"text-preset-2 pt-2 text-left text-white"}>£539,322.94</p>
+            </div>
           </div>
         </section>
       </div>
     </div>
   );
 }
+// /* Results */
+//
+// box-sizing: border-box;
+//
+// /* Auto layout */
+// display: flex;
+// flex-direction: column;
+// align-items: flex-start;
+// padding: 32px;
+// gap: 32px;
+//
+// width: 424px;
+// height: 293px;
+//
+// background: rgba(0, 0, 0, 0.25);
+// border-top: 4px solid #D8DB2F;
+// border-radius: 8px;
+//
+// /* Inside auto layout */
+// flex: none;
+// order: 1;
+// align-self: stretch;
+// flex-grow: 0;
