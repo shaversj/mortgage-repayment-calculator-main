3c576d2b95d96722053c2e015cb6222d285c27dd
Josh Shavers <shaversj@gmail.com>
2026-05-03 14:33:43 -0500

    Add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   server: {
@@ -16,5 +17,6 @@ export default defineConfig({
     // react's vite plugin must come after start's vite plugin
     viteReact(),
     tailwindcss(),
+    netlify(),
   ],
 });
